56f88e1d08c4c196437a6bcd87784b08e1f17d6c
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-14 13:53:30 +0200

    add term.php

---
+++ term.php
@@ -0,0 +1,20 @@
+<?php
+$taboo = $_GET['taboo'];
+$text = $_GET['text'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document 2</title>
+</head>
+
+<body>
+    <h1><?= $taboo ?></h1>
+    <h1><?= $text ?></h1>
+</body>
+
+</html>
